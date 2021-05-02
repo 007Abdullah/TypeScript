37b0c5112376cb4418537d9639e9f1e4467d3269
AbdullahKhan56 <AbdullahKhan56>
2021-05-02 05:29:57 +0500

    add

---
+++ src/sample.js
@@ -0,0 +1,5 @@
+console.log("hello world from TSC");
+function a(b) {
+    return b / 2;
+}
+a(2);
---
+++ src/sample.ts
@@ -0,0 +1,15 @@
+console.log("hello world from TSC");
+
+
+function abc(b: number) {
+    return b / 2
+}
+abc(2);
+abc(2);
+
+
+let firstName: string = "Sameer";
+let lastName: string = "Khan";
+let age: number = 45;
+let isActived: boolean = true;
+let course[]: [string] = ["sameer",]
